458f601152a5162b5bac07e50d0e8fa40afa6449
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-04-09 00:24:21 +0200

    First commit.

---
+++ src/main/java/com/lm/checktests/CheckTestsApplication.java
@@ -0,0 +1,13 @@
+package com.lm.checktests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lm/checktests/CheckTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.checktests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
