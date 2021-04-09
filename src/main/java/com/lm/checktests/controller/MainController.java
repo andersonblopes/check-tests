0514347a36194aab38369008909661c0930a4359
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-04-09 02:07:43 +0200

    Includes result view.

--- src/main/java/com/lm/checktests/controller/MainController.java
+++ src/main/java/com/lm/checktests/controller/MainController.java
@@ -1,29 +1,51 @@
 package com.lm.checktests.controller;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * The type Main controller.
  */
 @Controller
-@Getter
-@Setter
+@Data
 @Slf4j
 public class MainController {
 
+    private String message;
+
     /**
-     * Index string.
+     * Index model and view.
      *
-     * @param model the model
-     * @return the string
+     * @param modelAndView the model and view
+     * @return the model and view
      */
     @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        message = "Welcome to the Belgium!";
+        modelAndView.addObject("message", message);
+        return modelAndView;
     }
+
+    /**
+     * Result model and view.
+     *
+     * @param modelAndView the model and view
+     * @return the model and view
+     */
+    @GetMapping("/result")
+    public ModelAndView result(ModelAndView modelAndView){
+        modelAndView.setViewName("result-view");
+        message = "O resultado do processo seletivo";
+        modelAndView.addObject("message", message);
+        return modelAndView;
+    }
+
 }
